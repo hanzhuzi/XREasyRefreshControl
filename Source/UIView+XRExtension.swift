0d983df9b4800f3bcd4e6f64a430f508501327fa
hanzhuzi <1754410821@qq.com>
2018-07-11 18:28:27 +0800

    添加 spec文件

---
+++ Source/UIView+XRExtension.swift
@@ -0,0 +1,73 @@
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    var xr_y: CGFloat {
+        get {
+            return self.frame.origin.y
+        }
+        
+        set {
+            var tFrame = self.frame
+            tFrame.origin.y = newValue
+            self.frame = tFrame
+        }
+    }
+    
+    var xr_x: CGFloat {
+        get {
+            return self.frame.origin.x
+        }
+        
+        set {
+            var tFrame = self.frame
+            tFrame.origin.x = newValue
+            self.frame = tFrame
+        }
+    }
+    
+    var xr_height: CGFloat {
+        get {
+            return self.frame.size.height
+        }
+        
+        set {
+            var tFrame = self.frame
+            tFrame.size.height = newValue
+            self.frame = tFrame
+        }
+    }
+    
+    var xr_width: CGFloat {
+        get {
+            return self.frame.size.width
+        }
+        
+        set {
+            var tFrame = self.frame
+            tFrame.size.width = newValue
+            self.frame = tFrame
+        }
+    }
+    
+}
+
---
+++ XREasyRefreshControl.podspec
@@ -0,0 +1,25 @@
+#
+#  Be sure to run `pod spec lint XREasyRefreshControl.podspec' to ensure this is a
+#  valid spec and to remove all comments including this before submitting the spec.
+#
+#  To learn more about Podspec attributes see http://docs.cocoapods.org/specification.html
+#  To see working Podspecs in the CocoaPods repo see https://github.com/CocoaPods/Specs/
+#
+
+Pod::Spec.new do |s|
+
+  # ―――  Spec Metadata  ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
+
+  s.name         = "XREasyRefreshControl"
+  s.version      = "0.1"
+  s.summary      = "A simple and easy to use refresh control, you can customize your personality refresh according to the requirements."
+
+  s.homepage     = "https://github.com/hanzhuzi/XREasyRefreshControl"
+
+  s.license      = "MIT"
+  s.author             = { "hanzhuzi" => "1754410821@qq.com" }
+  s.ios.deployment_target = "8.0"
+  s.source       = { :git => "https://github.com/hanzhuzi/XREasyRefreshControl.git", :tag => s.version }
+  s.source_files  = 'Source/*.swift'
+
+end
